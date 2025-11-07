068f58f3bb85ea807dedcb382bac31d1eef26bba
Ellis Robinson <ellis@resolvergroup.com>
2025-11-07 11:41:54 +0000

    add create functionality to slots

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -7,4 +7,26 @@ class SlotsController < ApplicationController
     @slot = Slot.find(params[:id])
     @turners = @slot.turners
   end
+
+  def new
+    @slot = Slot.new
+  end
+
+  def create
+    @slot = Slot.new(slot_params)
+    if @slot.save
+      redirect_to @slot
+    else
+      render :new, staus: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def slot_params
+    params.require(:slot).permit(
+      :name,
+      :description
+    )
+  end
 end
